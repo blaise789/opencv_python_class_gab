c5da5a7997b5403b497ba3096cfd76eb5d49a3cb
blaise789 <alainiraguha2016@gmail.com>
2025-01-12 13:31:23 +0000

    updated the assignment.py file

--- convert_img_color.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img=cv2.imread("lena.jpg")
-img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("image show",img)
-cv2.waitKey(0)
-cv2.imwrite("lena_gray.png",img)
-cv2.destroyAllWindows()
